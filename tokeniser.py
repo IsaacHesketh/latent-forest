fb6a9f14c102a5377ede7e925e251bf56851ebe0
Isaac Hesketh <isaachesketh25@gmail.com>
2026-05-18 22:58:43 +0100

    added model architecture and tokeniser implementation

--- tokeniser.py
+++ tokeniser.py
@@ -1,5 +1,12 @@
 """
 Tokeniser functions
 """
+from dataset_loader import load_data
 def char_tokeniser():
     pass
+
+docs = load_data()
+
+uchars = sorted(set(''.join(docs))) # unique characters in the dataset become token ids 0..n-1
+BOS = len(uchars) # token id for the special Beginning of Sequence (BOS) token
+vocab_size = len(uchars) + 1 # total number of unique tokens, +1 is for BOS
